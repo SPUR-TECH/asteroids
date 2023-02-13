291b74370e9be09fc57e81e2e45521613fecd358
Scott Atkinson <scott.atkin79@gmail.com>
2023-02-13 13:48:33 +0000

    create enemies

--- index.js
+++ index.js
@@ -44,6 +44,28 @@ class Projectile {
     }
 }
 
+class Enemy {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
+    }
+    draw() {
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        c.fillStyle = this.color
+        c.fill()
+    }
+
+    update() {
+        this.draw()
+        this.x = this.x + this.velocity.x
+        this.y = this.y + this.velocity.y
+    }
+}
+
 const x = canvas.width / 2
 const y = canvas.height / 2
 
@@ -54,6 +76,40 @@ const player = new Player(
     'blue')
 
 const projectiles = []
+const enemies = []
+
+function spawnEnemies() {
+    setInterval(() => {
+            const radius = Math.random() * (50 - 4) + 4
+
+            let x
+            let y
+
+            if (Math.random() > 0.5) {
+                x = Math.random() > .5 ? 0 - radius : canvas.width + radius
+                y = Math.random() * canvas.height
+                // const y = Math.random() > .5 ? 0 - radius : canvas.height + radius
+            } else {
+                x = Math.random() * canvas.width
+                y = Math.random() > .5 ? 0 - radius : canvas.height + radius
+            }
+
+            const color = 'grey'
+
+            const angle = Math.atan2(
+                canvas.height / 2 - y,
+                canvas.width / 2 - x
+            )
+            const velocity = {
+                x: Math.cos(angle),
+                y: Math.sin(angle)
+            }
+
+            enemies.push(new Enemy(x, y, radius, color, velocity))
+            console.log(enemies)
+        },
+        1000)
+}
 
 function animate() {
     requestAnimationFrame(animate)
@@ -61,6 +117,10 @@ function animate() {
     player.draw()
     projectiles.forEach(projectile => {
         projectile.update()
+    })
+
+    enemies.forEach((enemy) => {
+        enemy.update()
     })
 }
 
@@ -97,3 +157,4 @@ addEventListener('touch', (event) => {
     )
 })
 animate()
+spawnEnemies()
