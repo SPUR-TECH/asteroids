26024087fa913dca1e8cdba5b4eea0f7cb38046e
Scott Atkinson <scott.atkin79@gmail.com>
2023-03-05 13:26:51 +0000

    create homing, spinning and homing spinning enemies

--- index.js
+++ index.js
@@ -84,9 +84,22 @@ class Enemy {
         this.color = color
         this.velocity = velocity
         this.type = 'Linier'
+        this.radians = 0
+        this.center = {
+            x,
+            y
+        }
 
         if (Math.random() > 0.5) {
             this.type = 'Homing'
+
+            if (Math.random() > 0.5) {
+                this.type = 'Spinning'
+            }
+
+            if (Math.random() > 0.5) {
+                this.type = 'Homing Spinning'
+            }
         }
     }
     draw() {
@@ -99,13 +112,39 @@ class Enemy {
     update() {
         this.draw()
 
-        if (this.type === 'Homing') {
+        if (this.type === 'Spinning') {
+            this.radians += 0.1
+
+            this.center.x += this.velocity.x
+            this.center.y += this.velocity.y
+
+            this.x = this.center.x + Math.cos(this.radians) * 30
+            this.y = this.center.y + Math.sin(this.radians) * 30
+
+        } else if (this.type === 'Homing') {
             const angle = Math.atan2(player.y - this.y, player.x - this.x)
             this.velocity.x = Math.cos(angle)
             this.velocity.y = Math.sin(angle)
+
+            this.x = this.x + this.velocity.x
+            this.y = this.y + this.velocity.y
+        } else if (this.type === 'Homing Spinning') {
+            this.radians += 0.1
+
+            const angle = Math.atan2(player.y - this.center.y, player.x - this.center.x)
+            this.velocity.x = Math.cos(angle)
+            this.velocity.y = Math.sin(angle)
+
+            this.center.x += this.velocity.x
+            this.center.y += this.velocity.y
+
+            this.x = this.center.x + Math.cos(this.radians) * 30
+            this.y = this.center.y + Math.sin(this.radians) * 30
+
+        } else {
+            this.x = this.x + this.velocity.x
+            this.y = this.y + this.velocity.y
         }
-        this.x = this.x + this.velocity.x
-        this.y = this.y + this.velocity.y
     }
 }
 
